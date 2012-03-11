ac728383fbfb25f10ebe547e1e35a101669141c7
Rainer W <github@inforw.de>
2012-03-11 14:03:46 +0100

    select database via properties

--- sonferenz-web/src/test/java/de/bitnosie/sonferenz/TestHomePage.java
+++ sonferenz-web/src/test/java/de/bitnosie/sonferenz/TestHomePage.java
@@ -1,31 +1,31 @@
-package de.bitnosie.sonferenz;
-
-import org.apache.wicket.util.tester.WicketTester;
-import org.junit.Before;
-
-import de.bitnoise.sonferenz.WicketApplication;
-import de.bitnoise.sonferenz.web.pages.statics.ConferencePage;
-
-/**
- * Simple test using the WicketTester
- */
-public class TestHomePage
-{
-  private WicketTester tester;
-
-  @Before
-  public void setUp()
-  {
-    tester = new WicketTester(new WicketApplication());
-  }
-
-  //@Test
-  public void homepageRendersSuccessfully()
-  {
-    // start and render the test page
-    tester.startPage(ConferencePage.class);
-
-    // assert rendered page class
-    tester.assertRenderedPage(ConferencePage.class);
-  }
-}
+//package de.bitnosie.sonferenz;
+//
+//import org.apache.wicket.util.tester.WicketTester;
+//import org.junit.Before;
+//
+//import de.bitnoise.sonferenz.WicketApplication;
+//import de.bitnoise.sonferenz.web.pages.statics.ConferencePage;
+//
+///**
+// * Simple test using the WicketTester
+// */
+//public class TestHomePage
+//{
+//  private WicketTester tester;
+//
+//  @Before
+//  public void setUp()
+//  {
+//    tester = new WicketTester(new WicketApplication());
+//  }
+//
+//  @Test
+//  public void homepageRendersSuccessfully()
+//  {
+//    // start and render the test page
+//    tester.startPage(ConferencePage.class);
+//
+//    // assert rendered page class
+//    tester.assertRenderedPage(ConferencePage.class);
+//  }
+//}
