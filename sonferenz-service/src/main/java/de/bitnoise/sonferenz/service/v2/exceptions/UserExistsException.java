efd01001ae250a2d9e1cef284473fdb6f65f137b
toxisch <toxisch@havre.de>
2012-03-26 16:17:55 +0200

    CrowdIdp does not check for existing users on createIdentity()

---
+++ sonferenz-service/src/main/java/de/bitnoise/sonferenz/service/v2/exceptions/UserExistsException.java
@@ -0,0 +1,11 @@
+package de.bitnoise.sonferenz.service.v2.exceptions;
+
+public class UserExistsException extends GeneralConferenceException
+{
+
+  public UserExistsException(String message)
+  {
+    super(message);
+  }
+
+}
