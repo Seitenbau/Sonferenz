44785461cf4f999398c8ed37e86b40c1c083495f
toxisch <toxisch@havre.de>
2012-04-16 11:31:14 +0200

    #53 Custom user text after login

---
+++ sonferenz-web/src/main/java/de/bitnoise/sonferenz/web/pages/statics/InfoPage.java
@@ -0,0 +1,25 @@
+package de.bitnoise.sonferenz.web.pages.statics;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.panel.Panel;
+
+import com.visural.wicket.aturl.At;
+
+import de.bitnoise.sonferenz.model.ConferenceModel;
+import de.bitnoise.sonferenz.web.pages.KonferenzPage;
+import de.bitnoise.sonferenz.web.pages.StaticContentPanel;
+
+@At(url = "/info")
+public class InfoPage extends KonferenzPage {
+	@Override
+	protected Panel getPageContent(String id) {
+		return new StaticContentPanel(id, "page.info");
+	}
+
+	public static PageParameters createParameters(ConferenceModel iModel) {
+		PageParameters pp = new PageParameters();
+		pp.add("info", String.valueOf(iModel.getId()));
+		return pp;
+	}
+
+}
